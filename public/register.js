7a7411874a468e359a162ea551bc726ab2c1b432
Hxgo <hugogamesbaeza@gmail.com>
2024-07-28 14:29:20 -0600

    modificacion de script de register para el cambio de idioma

--- public/register.js
+++ public/register.js
@@ -25,3 +25,74 @@ document.getElementById('register-form').addEventListener('submit', async (event
     alert('Error en el registro');
   }
 });
+
+const translations = {
+  en: {
+    "home-link": "Home",
+    "login-text": "Login",
+    "button": "Register",
+    "user_nombre": "Fist name",
+    "user_apellido": "Last name",
+    "user_password": "Password",
+    "user_email": "Email",
+    "h2-register-text": "User Register"
+  },
+  es: {
+    "home-link": "Inicio",
+    "login-text": "Iniciar Sesión",
+    "button": "Registrarse",
+    "user_nombre": "Nombre",
+    "user_apellido": "Apellido",
+    "user_password": "Contraseña",
+    "user_email": "Correo Electrónico",
+    "h2-register-text": "Registro de Usuario"
+  }
+};
+
+function changeLanguage(lang) {
+  const elements = document.querySelectorAll('[id]');
+  elements.forEach(element => {
+    const key = element.id;
+    if (translations[lang][key]) {
+      element.textContent = translations[lang][key];
+    }
+
+    // Change placeholders
+    const inputs = document.querySelectorAll('input[placeholder]');
+    inputs.forEach(input => {
+      const key = input.id;
+      if (translations[lang][key]) {
+        input.placeholder = translations[lang][key];
+      }
+    });
+
+    // Change select options
+    const selects = document.querySelectorAll('select');
+    selects.forEach(select => {
+      const key = select.id;
+      if (translations[lang][key]) {
+        const options = select.querySelectorAll('option');
+        options.forEach(option => {
+          if (option.value === "") {
+            option.textContent = translations[lang][key];
+          }
+        });
+      }
+    });
+  });
+
+  // Change language image
+  const languageImg = document.getElementById('language-img');
+  if (lang === 'es') {
+    languageImg.src = 'img/MXlenguaje.png';
+  } else {
+    languageImg.src = 'img/language.png';
+  }
+}
+
+document.getElementById('language-options').addEventListener('click', function (e) {
+  if (e.target.tagName === 'A') {
+    const lang = e.target.dataset.lang;
+    changeLanguage(lang);
+  }
+});
