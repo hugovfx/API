7271a3ad91e7747c1e1371ab1ebc4ae121487fad
StingerBlade <arras2805200@gmail.com>
2024-08-05 20:39:48 -0600

    datos de conexion actualizados

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'soloyo77',
-  database: 'dbMarketplace'
+  host: 'srvl 578.hstgr.io',
+  user: 'u589597310_gmartinez',
+  password: '>Cb67gLdO:',
+  database: 'u589597310_proyectoint'
 });
 
 db.connect((err) => {
